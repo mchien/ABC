34df3498168f1f113e12c0760ba9fe5bb1279493
Michael <michael.t.ch@outlook.com>
2018-05-08 13:30:49 -0700

    initial
    initial commit

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var now = System.DateTime.Now;
+        var s = now.Second;
+        var ms = now.Millisecond;
+
+        var sFactor = 360f / 60f;
+        var mFactor = 6f / 1000f;
+
+        var degrees = s * sFactor + ms * mFactor;
+
+        transform.rotation = Quaternion.Euler(degrees * speed * -1, transform.rotation.y, transform.rotation.z);
+    }
+}
