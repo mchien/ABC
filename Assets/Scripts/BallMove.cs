b5e17c18451f9f8e466016390e457d29790ea7eb
Michael <michael.t.ch@outlook.com>
2018-05-10 15:47:38 -0700

    walls, ball, paddle and reflecting behaviour

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+
+	public float thrust = 500;
+    private Rigidbody rb;
+    private Vector3 preVelocity;
+    private Vector3 postVelocity;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(0, -thrust, 0, ForceMode.Force);
+    }
+
+    void FixedUpdate()
+    {
+        preVelocity = rb.velocity;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = Vector3.Reflect(preVelocity, collision.contacts[0].normal);
+    }
+}
---
+++ Assets/Scripts/WallPosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class WallPosition : MonoBehaviour {
+    private bool positionSet = false;
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(SetWalls());        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator SetWalls() {
+        yield return new WaitForSeconds(0.2f);
+        if (!positionSet) {
+            if (VuforiaBehaviour.Instance.enabled) {
+                Transform cam = Camera.main.transform;
+
+                transform.position = new Vector3(
+                    cam.right.x * transform.position.x, 
+                    cam.up.y * transform.position.y, 
+                    cam.forward.z * transform.position.z
+                    );
+
+
+                positionSet = true;
+            }
+        }
+    }
+}
